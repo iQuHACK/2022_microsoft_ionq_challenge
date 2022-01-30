9940f418968798f8343311d7571f8d5d69e0019e
Alessandro Credidio Furlan <64380379+alessandrofurlan@users.noreply.github.com>
2022-01-30 12:50:07 -0300

    Update config.py

--- app/services/block/config.py
+++ app/services/block/config.py
@@ -6,9 +6,9 @@ possible_operators = [
     {'name': 'X', 'obj': Pauli(label='X'), 'img_path': '../../static/img/X.gif'},
     {'name': 'Y', 'obj': Pauli(label='Y'), 'img_path': '../../static/img/Y.gif'},
     {'name': 'Z', 'obj': Pauli(label='Z'), 'img_path': '../../static/img/Z.gif'},
-    {'name': 'Rx', 'obj': RXGate(np.pi/2), 'img_path': '../../static/img/Rx.gif'},
-    {'name': 'Ry', 'obj': RYGate(np.pi/2), 'img_path': '../../static/img/Ry.gif'},
-    {'name': 'Rz', 'obj': RZGate(np.pi/2), 'img_path': '../../static/img/Rz.gif'},
+    {'name': 'Rx', 'obj': RXGate(np.pi), 'img_path': '../../static/img/Rx.gif'},
+    {'name': 'Ry', 'obj': RYGate(np.pi), 'img_path': '../../static/img/Ry.gif'},
+    {'name': 'Rz', 'obj': RZGate(np.pi), 'img_path': '../../static/img/Rz.gif'},
     {'name': 'H', 'obj': HGate(), 'img_path': '../../static/img/H.gif'},
     {'name': 'I', 'obj': IGate(), 'img_path': '../../static/img/I.gif'}
 ] 
